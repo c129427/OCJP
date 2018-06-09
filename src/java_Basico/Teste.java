adfc2c0a896bbbba736ccda47ac45214f83d1f82
c129427 <36902864+c129427@users.noreply.github.com>
2018-06-09 15:44:20 -0300

    primeiro comit do projet OCJP
    apenas teste básico

---
+++ src/java_Basico/Teste.java
@@ -0,0 +1,23 @@
+package java_Basico;
+
+import javax.swing.JOptionPane;
+
+public class Teste {
+
+	public void m1() {
+		int x = 10;
+		System.out.println(x);
+		
+	}
+	public static void main(String[] args) {
+		Teste t = new Teste();
+
+		t.m1();
+		
+		System.out.println("Hello World!!");
+		
+		JOptionPane.showMessageDialog(null,"");
+
+	}
+
+}
